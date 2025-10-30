6ab2f17868a67d72f64641866a1fc1d8161ad0e1
brendaivana <bren12.benitez@gmail.com>
2025-10-30 00:55:32 -0300

    Update TP7Ejercicios.py

--- TP7Ejercicios.py
+++ TP7Ejercicios.py
@@ -1,18 +1,16 @@
 # Ejercicio 1
-# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
 
-# precios_frutas["Naranja"] = 1200
-# precios_frutas["Manzana"] = 1500
-# precios_frutas["Pera"] = 2300
-
-# print(precios_frutas)
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+print(precios_frutas)
 
 #Ejercicio 2
-# precios_frutas['Banana'] = 1330
-# precios_frutas['Manzana'] = 1700
-# precios_frutas['Melón'] = 2800
-
-# print(precios_frutas)
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] = 2800
+print(precios_frutas)
 
 #Ejercicio 3
 # frutas = list(precios_frutas.keys())
@@ -121,3 +119,4 @@ print(original)
 
 print("\nDiccionario diferente:")
 print(diferente)
+
